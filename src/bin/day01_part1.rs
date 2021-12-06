639a1335b3edb4f34649e6f02740898bd0680b48
Maël Obréjan <mael.obrejan@protonmail.com>
2021-12-06 14:07:03 +0100

    Rewrite day 1 solutions with fold

--- src/bin/day01_part1.rs
+++ src/bin/day01_part1.rs
@@ -2,21 +2,20 @@
 /// Count the number of times a depth measurement increases from the previous measurement.
 use std::io::{self, BufRead};
 
+use anyhow::Ok;
+
 fn main() -> Result<(), anyhow::Error> {
-    let mut increases = 0;
-    let mut previous_depth = None;
+    let (_last_depth, increases) = io::stdin().lock().lines().try_fold(
+        (None, 0_u32),
+        |(previous_depth, increases), depth| {
+            let depth = depth?.parse::<u64>()?;
 
-    for line in io::stdin().lock().lines() {
-        let depth = line?.parse::<u64>()?;
-
-        if let Some(previous_depth) = previous_depth {
-            if depth > previous_depth {
-                increases += 1;
+            match previous_depth {
+                Some(previous_depth) if depth > previous_depth => Ok((Some(depth), increases + 1)),
+                _ => Ok((Some(depth), increases)),
             }
-        }
-
-        previous_depth = Some(depth);
-    }
+        },
+    )?;
 
     println!("Number of increases: {}", increases);
 
--- src/bin/day01_part2.rs
+++ src/bin/day01_part2.rs
@@ -1,34 +1,28 @@
 /// Day 1
 /// Count the number of times a depth measurement increases from the previous measurement, over a
 /// sliding window of three measurements.
-use std::{
-    collections::VecDeque,
-    io::{self, BufRead},
-};
+use std::io::{self, BufRead};
+
+use anyhow::Ok;
 
 fn main() -> Result<(), anyhow::Error> {
-    let mut increases = 0;
-    let mut window = VecDeque::with_capacity(3);
-    let mut previous_window_sum = None;
+    let (_last_window_sum, increases) = io::stdin()
+        .lock()
+        .lines()
+        .collect::<Result<Vec<_>, _>>()?
+        .windows(3)
+        .try_fold((None, 0_u64), |(previous_window_sum, increases), window| {
+            let window_sum = window
+                .iter()
+                .try_fold(0_u64, |sum, depth| Ok(sum + depth.parse::<u64>()?))?;
 
-    for line in io::stdin().lock().lines() {
-        let depth = line?.parse::<u64>()?;
-
-        window.push_back(depth);
-
-        if window.len() >= 3 {
-            let sum: u64 = window.iter().sum();
-
-            if let Some(previous_window_sum) = previous_window_sum {
-                if sum > previous_window_sum {
-                    increases += 1;
+            match previous_window_sum {
+                Some(previous_window_sum) if window_sum > previous_window_sum => {
+                    Ok((Some(window_sum), increases + 1))
                 }
+                _ => Ok((Some(window_sum), increases)),
             }
-
-            previous_window_sum = Some(sum);
-            window.pop_front();
-        }
-    }
+        })?;
 
     println!("Number of increases: {}", increases);
 
